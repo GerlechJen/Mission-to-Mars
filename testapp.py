5763534108f14a9bce0f897b42a13d7e0be5beaf
Jennifer Anno-Kusi <jannokusi@gmail.com>
2022-07-09 19:03:59 -0400

    Adding updated files

---
+++ testapp.py
@@ -0,0 +1,75 @@
+{
+ "cells": [
+  {
+   "cell_type": "code",
+   "execution_count": 1,
+   "id": "a13b45c5",
+   "metadata": {},
+   "outputs": [
+    {
+     "name": "stdout",
+     "output_type": "stream",
+     "text": [
+      " * Serving Flask app \"__main__\" (lazy loading)\n",
+      " * Environment: production\n",
+      "\u001b[31m   WARNING: This is a development server. Do not use it in a production deployment.\u001b[0m\n",
+      "\u001b[2m   Use a production WSGI server instead.\u001b[0m\n",
+      " * Debug mode: off\n"
+     ]
+    },
+    {
+     "name": "stderr",
+     "output_type": "stream",
+     "text": [
+      " * Running on http://127.0.0.1:5000/ (Press CTRL+C to quit)\n"
+     ]
+    }
+   ],
+   "source": [
+    "from flask import Flask, render_template, redirect, url_for\n",
+    "from flask_pymongo import PyMongo\n",
+    "import scraping\n",
+    "app = Flask(__name__)\n",
+    "# Use flask_pymongo to set up mongo connection\n",
+    "app.config[\"MONGO_URI\"] = \"mongodb://localhost:27017/mars_app\"\n",
+    "mongo = PyMongo(app)\n",
+    "\n",
+    "@app.route(\"/\")\n",
+    "def index():\n",
+    "   mars = mongo.db.mars.find_one()\n",
+    "   return render_template(\"index.html\", mars=mars)\n",
+    "\n",
+    "@app.route(\"/scrape\")\n",
+    "def scrape():\n",
+    "   mars = mongo.db.mars\n",
+    "   mars_data = scraping.scrape_all()\n",
+    "   mars.update_one({}, {\"$set\":mars_data}, upsert=True)\n",
+    "   return redirect('/', code=302)\n",
+    "\n",
+    "if __name__ == \"__main__\":\n",
+    "   app.run()"
+   ]
+  }
+ ],
+ "metadata": {
+  "kernelspec": {
+   "display_name": "PythonData",
+   "language": "python",
+   "name": "pythondata"
+  },
+  "language_info": {
+   "codemirror_mode": {
+    "name": "ipython",
+    "version": 3
+   },
+   "file_extension": ".py",
+   "mimetype": "text/x-python",
+   "name": "python",
+   "nbconvert_exporter": "python",
+   "pygments_lexer": "ipython3",
+   "version": "3.7.13"
+  }
+ },
+ "nbformat": 4,
+ "nbformat_minor": 5
+}
